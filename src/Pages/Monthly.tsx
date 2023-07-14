497a20b66944a3c405f01691f12057ee5fc091be
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-07-14 23:18:57 +0200

    Added mew files to the layout

---
+++ src/Pages/Monthly.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Monthly extends Component {
+  render() {
+    return <div>Monthly</div>;
+  }
+}
+
+export default Monthly;
---
+++ src/Pages/Weekly.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Weekly() {
+  return <div>Weekly</div>;
+}
