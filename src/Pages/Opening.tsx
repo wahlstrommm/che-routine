83a7f312731c480332a85c07702063fc7bf12e9f
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-07-15 23:08:22 +0200

    started on the opening comp and with axios, json

--- src/Pages/Opening.tsx
+++ src/Pages/Opening.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Routine } from "./Types"; // Ange den korrekta sökvägen till din types.ts-fil
+const Opening = () => {
+  const [routine, setRoutine] = useState<Routine | null>(null); // Använd den definierade typen här
 
-function Opening() {
-  return <div>Opening</div>;
-}
+  useEffect(() => {
+    const fetchRoutines = async () => {
+      try {
+        const response = await axios.get("../assets/openingRoutine.json");
+        setRoutine(response.data as Routine);
+      } catch (error) {
+        console.error("Error fetching opening routines:", error);
+      }
+    };
+
+    fetchRoutines().catch((error) => {
+      console.error("Unhandled promise rejection:", error);
+    });
+  }, []);
+  return (
+    <div>
+      <h2>Öppningsrutin</h2>
+      {routine && (
+        <ul>
+          {Object.entries(routine).map(([key, value]) => (
+            <li key={key}>
+              {key}: {value}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Opening;
