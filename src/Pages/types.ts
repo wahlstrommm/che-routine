efafd85ab49b93e02af397c82b488556465dca8e
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-07-15 23:10:32 +0200

    Interface for the json file that create routines

---
+++ src/Pages/types.ts
@@ -0,0 +1,9 @@
+export interface Routine {
+  // Ange strukturen för rutinlistan baserat på dina data
+  // Exempelvis: 
+  // property1: string;
+  // property2: number;
+  // ...
+  // Justera strukturen efter dina behov.
+  [property: string]: any;
+}
