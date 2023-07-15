d36a312d62872ed6a4b112d0ce2ba812f3e21959
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-07-15 22:37:51 +0200

    Finsed the layout and the routes

--- src/Pages/Layout.tsx
+++ src/Pages/Layout.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import "../App.css";
 
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/Opening">Öppningsrutin</Link>
+          </li>
+          <li>
+            <Link to={"/Closning"}>Stängningsrutin</Link>
+          </li>
+          <li>
+            <Link to={"/Weekly"}>Veckorutin</Link>
+          </li>
+          <li>
+            <Link to="/Monthly">Månadsrutin</Link>
+          </li>
+          <li>
+            <Link to="/Special">Special</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
 }
 
 export default Layout;
--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,25 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter, Router, Link, Route, Routes } from "react-router-dom";
 import Opening from "./Pages/Opening.tsx";
+import NoPage from "./Pages/NoPage.tsx";
+import Weekly from "./Pages/Weekly.tsx";
+import Monthly from "./Pages/Monthly.tsx";
+import Closning from "./Pages/Closning.tsx";
+import Layout from "./Pages/Layout.tsx";
+import Special from "./Pages/Special.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/Opening" element={<Opening />} />
-        <Route path="*" element={<App />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/Opening" element={<Opening />} />
+          <Route path="/Closning" element={<Closning />} />
+          <Route path="Monthly" element={<Monthly />} />
+          <Route path="Weekly" element={<Weekly />} />
+          <Route path="Special" element={<Special />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
