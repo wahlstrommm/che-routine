4499198eab12bec20ecff24ed4565304a1333644
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-09-24 22:31:56 +0200

    removed import

--- src/Pages/PastRoutines.tsx
+++ src/Pages/PastRoutines.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 export default function PastRoutines() {
   return <div>This will show the list of past routines</div>;
--- src/Pages/Special.tsx
+++ src/Pages/Special.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Special() {
   return <div>Special</div>;
 }
