15d631f060e05c04c09297c106acddec92a6af75
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-03 12:36:43 +0100

    updated the interface

--- src/Types/IResponseData.ts
+++ src/Types/IResponseData.ts
@@ -3,8 +3,8 @@ import { Routine } from "../Types/types";
 interface ResponseData {
     Namn: string;
     Datum: string;
-    Rutiner: Routine[]; // replace 'any' with the actual type of Rutiner if possible
-    SenastSparad: string; // Update the type for SenastSparad
+    Rutiner?: Routine[] | undefined; // 
+    SenastSparad: string; 
     Anledning: string;
     index: number;
     // ... other properties ...
