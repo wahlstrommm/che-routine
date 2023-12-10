5dcab6be619b86dcec9a9de5ff3d4328189d08cb
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-10 10:21:13 +0100

    Finished the overall func in summary

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -1,3 +1,27 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Routine } from "../Types/types";
+
 export default function Summary() {
-  return <div>This will show the list of past routines test</div>;
+  const [summaryData, setSummaryData] = useState<Routine[]>([]);
+  useEffect(() => {
+    axios
+      .get("/api/routines/summary")
+      .then((response) => setSummaryData(response.data as Routine[]))
+      .catch((error) => console.error("Error fetching routine summary", error));
+  }, []);
+  return (
+    <div>
+      {/* Exempel: Visa månader och antal klara rutiner */}
+      {summaryData.map((month) => (
+        <div key={month.name as string}>
+          <h3>{month.name}</h3>
+          <p>{`${month.completed as string} av ${
+            month.total as string
+          } rutiner klara`}</p>
+          {/* Lägg till interaktivitet här för att visa veckor och dagar */}
+        </div>
+      ))}
+    </div>
+  );
 }
