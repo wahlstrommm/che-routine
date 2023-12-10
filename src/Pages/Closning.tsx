303500637dbb12102dcc5a38715e454b80f60828
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-10 10:32:29 +0100

    imports

--- src/Pages/Closning.tsx
+++ src/Pages/Closning.tsx
@@ -1,3 +1,8 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "react-overlays/Modal";
+import { Routine } from "../Types/types";
+
 export default function Closning() {
   return <div>Closning</div>;
 }
