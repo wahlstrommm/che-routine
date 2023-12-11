bdd47367abfc3f920c7be7335b2cfeb6afabb3ec
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-11 22:53:28 +0100

    Created route

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import Closning from "./Pages/Closning.tsx";
 import Layout from "./Pages/Layout.tsx";
 import Special from "./Pages/Special.tsx";
 import Summary from "./Pages/Summary.tsx";
+import Upload from "./Pages/Upload.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -22,6 +23,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
           <Route path="Weekly" element={<Weekly />} />
           <Route path="Special" element={<Special />} />
           <Route path="/Summary" element={<Summary />} />
+          <Route path="/upload" element={<Upload />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
