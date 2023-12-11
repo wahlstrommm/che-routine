118da95efefd0e16efd3761ade986f94c8531fb0
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-11 23:04:55 +0100

    started on the upload

--- src/Pages/Upload.tsx
+++ src/Pages/Upload.tsx
@@ -1,5 +1,35 @@
 import React, { useState } from "react";
 
 export default function Upload() {
-  return <div>Upload</div>;
+  const [file, setFile] = useState<File | null>(null);
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFile(e.target.files[0]);
+    }
+  };
+  const handleUpload = () => {
+    // TODO
+  };
+  return (
+    <>
+      <div>
+        <label htmlFor="file" className="sr-only">
+          Choose a file
+        </label>
+        <input id="file" type="file" onChange={handleFileChange} />
+      </div>
+      {file && (
+        <section>
+          File details:
+          <ul>
+            <li>Name: {file.name}</li>
+            <li>Type: {file.type}</li>
+            <li>Size: {file.size} bytes</li>
+          </ul>
+        </section>
+      )}
+
+      {file && <button onClick={handleUpload}>Upload a file</button>}
+    </>
+  );
 }
