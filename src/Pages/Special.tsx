144bcc3ac1da79dc2e0c3d5c425dd2a344914bed
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-14 22:04:27 +0100

    imported resct,sestate,usestate, axios

--- src/Pages/Special.tsx
+++ src/Pages/Special.tsx
@@ -1,3 +1,8 @@
+import React, { SetStateAction, useEffect, useState } from "react";
+import { Routine } from "../Types/types";
+import Modal from "react-overlays/Modal";
+import axios from "axios";
+
 export default function Special() {
   return <div>Special</div>;
 }
