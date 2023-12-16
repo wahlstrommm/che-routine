fe5b84d7ff3743de182311f2548bd6c958c82d9c
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-16 23:58:19 +0100

    import the right modules

--- src/Pages/Monthly.tsx
+++ src/Pages/Monthly.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
+import { Routine } from "../Types/types";
+import Modal from "react-overlays/Modal";
+import axios from "axios";
 
 export default function Monthly() {
   const [name, setName] = useState("");
