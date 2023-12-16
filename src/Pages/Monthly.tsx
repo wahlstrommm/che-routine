fc568b2389c946acdadb78798eb1ac8761c3514b
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-16 23:54:59 +0100

    handleOverlayButtonClick

--- src/Pages/Monthly.tsx
+++ src/Pages/Monthly.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
 export default function Monthly() {
+  const handleOverlayButtonClick = () => {
+    setShowModal(true);
+  };
+
   const getData = async () => {
     try {
       const response = await axios.get(
