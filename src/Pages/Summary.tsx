4f36d95ed7d636d8b036a7cac442de0418a16e09
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-31 00:27:52 +0100

    updated the map function

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -7,21 +7,28 @@ export default function Summary() {
   useEffect(() => {
     axios
       .get("http://localhost:3000/summary")
-      .then((response) => setSummaryData(response.data as Routine[]))
+      .then((response) => {
+        console.log(response.data); // Check the response data in the console
+        setSummaryData(response.data as Routine[]);
+      })
       .catch((error) => console.error("Error fetching routine summary", error));
   }, []);
   return (
     <div>
       {/* Exempel: Visa månader och antal klara rutiner */}
-      {summaryData.map((month) => (
-        <div key={month.name as string}>
-          <h3>{month.name}</h3>
-          <p>{`${month.completed as string} av ${
-            month.total as string
-          } rutiner klara`}</p>
-          {/* Lägg till interaktivitet här för att visa veckor och dagar */}
-        </div>
-      ))}
+      {Array.isArray(summaryData) && summaryData.length > 0 ? (
+        summaryData.map((month) => (
+          <div key={month.name as string}>
+            <h3>{month.name}</h3>
+            <p>{`${month.completed as string} av ${
+              month.total as string
+            } rutiner klara`}</p>
+            {/* Lägg till interaktivitet här för att visa veckor och dagar */}
+          </div>
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
     </div>
   );
 }
