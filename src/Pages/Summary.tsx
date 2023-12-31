eeab900a70a4a3b36ce2460e84d2b1d15e665b12
Magnus Vahlström <Wahlstrommm@gmail.com>
2023-12-31 01:10:22 +0100

    Updated the summary for the layout

--- src/Pages/Summary.tsx
+++ src/Pages/Summary.tsx
@@ -3,29 +3,54 @@ import axios from "axios";
 import { Routine } from "../Types/types";
 
 export default function Summary() {
-  const [summaryData, setSummaryData] = useState<Routine[]>([]);
+  const [summaryData, setSummaryData] = useState<Routine[][][]>([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:3000/summary")
       .then((response) => {
         console.log(response.data); // Check the response data in the console
-        setSummaryData(response.data as Routine[]);
+        setSummaryData(response.data as Routine[][][]);
       })
       .catch((error) => console.error("Error fetching routine summary", error));
   }, []);
+
+  const renderDays = (days: Routine[]) => (
+    <ul>
+      {days.map((day) => (
+        <li key={day.id}>{day.name}</li>
+      ))}
+    </ul>
+  );
+
+  const renderWeeks = (weeks: Routine[][]) => (
+    <div>
+      {weeks.map((week, weekIndex) => (
+        <div key={weekIndex}>
+          <h4>Week {weekIndex + 1}</h4>
+          {renderDays(week)}
+        </div>
+      ))}
+    </div>
+  );
+
+  const renderMonths = (months: Routine[][][] = []) => (
+    <div>
+      {months.map((month, monthIndex) => (
+        <div key={monthIndex}>
+          {/* Assuming 'date' is on each day object within the month */}
+          <h3>{month[0].day}</h3>
+          {renderWeeks(month)}
+        </div>
+      ))}
+    </div>
+  );
+
   return (
     <div>
-      {/* Exempel: Visa månader och antal klara rutiner */}
+      {/* Example: Display months, weeks, and days */}
       {Array.isArray(summaryData) && summaryData.length > 0 ? (
-        summaryData.map((month) => (
-          <div key={month.name as string}>
-            <h3>{month.name}</h3>
-            <p>{`${month.completed as string} av ${
-              month.total as string
-            } rutiner klara`}</p>
-            {/* Lägg till interaktivitet här för att visa veckor och dagar */}
-          </div>
-        ))
+        renderMonths(summaryData)
       ) : (
         <p>No data available</p>
       )}
